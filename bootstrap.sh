87f30d287ae3e476e7372fbb46fcf689441feb26
trannerite <stoleurwarezlmao@protonmail.com>
2024-11-02 21:52:09 +0000

    Removed "VAC Bypass"

--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo mv sentinelguard /usr/local/bin/sentinelguard && sudo chown root:root /usr/local/bin/sentinelguard && sudo chmod +sx /usr/local/bin/sentinelguard && echo "Installation done, you can now use sentinelguard from anywhere." && sentinelguard --help
